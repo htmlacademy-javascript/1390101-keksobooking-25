6ec9ceff838f5146085844c9f81a2a7cfb161587
Kristina <--replace-all>
2022-02-28 14:19:54 +0300

    добавляет вторую функцию

--- js/main.js
+++ js/main.js
@@ -5,21 +5,28 @@
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-if(max>min)
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-if (min=max) {
-  return min;
+  if(max>min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-else
-  {return 'Значение От больше значения До'}
+  if (min=max) {
+    return min;
+  }
+  else {return 'Значение От больше значения До';}
+}
 
 /* 2 Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
   диапазон может быть только положительный, включая ноль.
   как функция должна вести себя, если передать значение «до» меньшее, чем значение «от», или равное ему.
 */
 
-function getRandom(number) {
-  return Math.floor(Math.random()*number);
+function getRandom(min, max, random) {
+  if (min < 0 || max < 0) {
+    throw new Error('Числа должны быть только положительными');
+  }
+  if (min < max) {
+    return (Math.random() * (max - min) + min).toFixed(random);
+  }
+  else {
+    return (Math.random() * (min - max) + max).toFixed(random);
+  }
 }
-
