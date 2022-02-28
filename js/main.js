243d305e66e8989cc840f68537f4df61d2b303c0
Kristina <--replace-all>
2022-02-28 22:37:42 +0300

    Исправляет функции

--- js/main.js
+++ js/main.js
@@ -2,31 +2,34 @@
   диапазон может быть только положительный, включая ноль.
   как функция должна вести себя, если передать значение «до» меньшее, чем значение «от», или равное ему.*/
 
-function getRandomIntInclusive(min, max) {
+function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if(max>min) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if (min > max || min < 0 || max < 0) {
+    throw new Error('Неверные параметры функции');
   }
-  if (min=max) {
-    return min;
-  }
-  else {return 'Значение От больше значения До';}
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+getRandomInt(1, 8);
 
 /* 2 Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
   диапазон может быть только положительный, включая ноль.
   как функция должна вести себя, если передать значение «до» меньшее, чем значение «от», или равное ему.
 */
 
-function getRandom(min, max, random) {
+function getRandomFloat(min, max, fractionDigits) {
   if (min < 0 || max < 0) {
     throw new Error('Числа должны быть только положительными');
   }
+
   if (min < max) {
-    return (Math.random() * (max - min) + min).toFixed(random);
+    return (Math.random() * (max - min) + min).toFixed(fractionDigits);
   }
-  else {
-    return (Math.random() * (min - max) + max).toFixed(random);
-  }
+
+  return (Math.random() * (min - max) + max).toFixed(fractionDigits);
 }
+
+getRandomFloat(1.25, 4.35, 5);
