e566e4e5764387110a757d97c0a59f07a73330c4
Kristina <--replace-all>
2022-03-18 23:58:02 +0300

    Добавляет генерацию данных

--- js/main.js
+++ js/main.js
@@ -33,3 +33,107 @@ function getRandomFloat(min, max, fractionDigits) {
 }
 
 getRandomFloat(1.25, 4.35, 5);
+
+
+const TITLES = [
+  'Норка в тихом лесу.',
+  'Коммуналка на Мойке.',
+  'Студия в 25-этажке.',
+  'Просторная квартира в Мурино.',
+  'Бунгало на Мальдивах.',
+  'Евродвушка на Крестовском',
+  'Аппартаменты у аэропорта',
+  'Пансионат Шишки на Лампушках'
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const DESCRIPTIONS = [
+  'простор и уют обеспечены',
+  'тихие соседи ведущие ассоциальный образ жизни',
+  'идеально для семьи с тремя детьми',
+  'каменные джунгли в вашем распоряжении',
+  'джунам не потянуть',
+  'для экономных и не притязательных',
+  'не пожалеете и хорошо отдохнете',
+  'если желаете отдохнуть от интернета и сотовой связи, вам сюда',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+const GENERATED_OBJ = 10;
+
+const LOCATIONS = () => ({
+  lat: getRandomFloat(35.65000, 35.70000, 5),
+  ing: getRandomFloat(139.70000, 139.80000, 5)
+});
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomPositiveInteger(0, elements.length -1)];
+};
+
+const createOffer = () => {
+  const offer = {
+    autor: {
+      avatar: 'img/avatars/${userId}.png'
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: lat, ing(LOCATIONS),
+      price: getRandomInt(1, 1000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomInt(1, 5),
+      guests: getRandomInt(1, 20),
+      checkin: getRandomArrayElement(CHECKINS),
+      checkout: getRandomArrayElement(CHECKOUTS),
+      features: getRandomArrayElement(FEATURES),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getRandomArrayElement(PHOTOS),
+    },
+    location: {
+      lat: lat,
+      ing: ing
+    }
+  }
+};
+
+const createOffers = () => {
+  const data = [];
+
+  for (let i = 0; i < 10; i++) {
+    data.push(createOffer());
+  }
+
+  return data;
+};
